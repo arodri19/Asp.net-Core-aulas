305a545f7da122ff2f34a8dd10643ff95973d775
André Rodrigues <andre.rodrigues19@outlook.com.br>
2018-04-24 22:10:31 -0300

    Aula 06

---
+++ Aula2/CasaDoCodigo/ContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Aula2
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<Contexto>
+    {
+        public Contexto CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            return new Contexto(optionsBuilder.Options);
+        }
+    }
+}
--- Aula2/CasaDoCodigo/Startup.cs
+++ Aula2/CasaDoCodigo/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CasaDoCodigo.Aula2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,6 +31,8 @@ namespace CasaDoCodigo
         {
             // Add framework services.
             services.AddMvc();
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<Contexto>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
